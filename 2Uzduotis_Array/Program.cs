87abe73f248823653bc11f83314b494158fd348d
Laura <Laura@DESKTOP-BESSB6E>
2023-09-18 23:19:36 +0300

    Add project files.

---
+++ 2Uzduotis_Array/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace _2Uzduotis_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "Katinas";
+            GetCharArray(word);
+
+        }
+        public static void GetCharArray(string word)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                Console.WriteLine(word[i]);
+            }
+        }
+    }
+}
